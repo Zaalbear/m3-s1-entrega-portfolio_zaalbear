d55570464d4c9a0ba50c6826d4539ac45828bf81
Zaalbear2 <zaalbear@gmail.com>
2023-09-25 23:56:54 -0300

    fix: adicionado a children-props em PageTamplate.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { PageTamplate } from "./pages/PageTamplate"
+import { BannerSection } from "./components/BannerSection";
+import { AboutMeSection } from "./components/AboutMeSection";
+import { TechSection } from "./components/TechSection";
+import { ProjectsSection } from "./components/ProjectsSection";
 
 function App() { 
   return (
     <>
-      <PageTamplate/>
+      <PageTamplate>
+        <BannerSection />
+        <AboutMeSection />
+        <TechSection />
+        <ProjectsSection />
+      </PageTamplate>
     </>
   ) }
 
--- src/pages/PageTamplate.jsx
+++ src/pages/PageTamplate.jsx
@@ -1,27 +1,18 @@
 //Estilos
-import "../styles/reset.css"
-import "../styles/globalStyles.css"
-import styles from "./pageStyle.module.css"
+import "../styles/reset.css";
+import "../styles/globalStyles.css";
+import styles from "./pageStyle.module.css";
 
 //Componentes
-import { Header } from "../components/Header"
-import { BannerSection } from "../components/BannerSection"
-import { AboutMeSection } from "../components/AboutMeSection"
-import { TechSection } from "../components/TechSection"
-import { ProjectsSection } from "../components/ProjectsSection"
-import { Footer } from "../components/Footer"
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
-
-
-export const PageTamplate = () => {
+export const PageTamplate = ({ children }) => {
   return (
     <div className={styles.page__container}>
       <Header />
       <main className={styles.main__container}>
-        <BannerSection />
-        <AboutMeSection />
-        <TechSection />
-        <ProjectsSection />
+        {children}
       </main>
       <Footer />
     </div>
